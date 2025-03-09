0e8f23e10d8c16c36d72f000c7cccfd6885f9319
M Vishnu vardhan <vishnuvardhan7823@gmail.com>
2025-03-09 10:18:34 +0530

    final project done

---
+++ Project/Backend/index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const app = express();
+const dotenv = require('dotenv');
+const cors = require('cors');
+const db = require('./src/Config/db')
+dotenv.config();
+const routes = require('./src/routes/authRoutes')
+const documentRoutes = require('./src/routes/documentRoutes');
+const creditsRoutes = require('./src/routes/creditRoutes');
+const cron = require('node-cron');
+const {resetDailycredits} = require('./src/Controllers/creditController');
+const path = require('path');
+const adminRoutes =require('./src/routes/adminRoutes')
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public")); 
+
+//routes
+
+
+app.use('/api/auth',routes);
+app.use('/api/documents',documentRoutes);
+app.use('/api/credits',creditsRoutes);
+app.use('/api/admin',adminRoutes);
+
+
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname, '../Frontend/index.html'));
+
+})
+
+//reset credits every midnight
+cron.schedule("0 0 * * *", resetDailycredits);
+
+app.use(express.static(path.join(__dirname, '../Frontend')));
+
+
+app.listen(process.env.PORT,()=>{
+    console.log(`server is connected to ${process.env.PORT}`);
+})
+
---
+++ Project/Backend/src/adminpasshash.js
@@ -0,0 +1,23 @@
+const bcrypt = require("bcrypt");
+const sqlite3 = require("sqlite3").verbose();
+
+const db = new sqlite3.Database('./src/data/database.sqlite');
+
+const username = "admin";
+const plainPassword = "admin123";
+
+bcrypt.hash(plainPassword, 10, (err, hash) => {
+    if (err) {
+        console.error("Error hashing password:", err);
+        return;
+    }
+    
+    db.run(`UPDATE users SET password = ? WHERE username = ?`, [hash, username], (err) => {
+        if (err) {
+            console.error("Error updating admin password:", err);
+        } else {
+            console.log("Admin password updated successfully.");
+        }
+        db.close();
+    });
+});
